4da72dad16d7727ab5fe16b4e7ef01b1d6f1fd45
Assembly98 <doodleymail98@gmail.com>
2025-12-02 17:32:46 -0800

    physics addition
    modified and added to the physics script
    
    lololololololololololololololololol

--- protoPhysicsR.gd
+++ protoPhysicsR.gd
@@ -1,14 +1,21 @@
 extends RigidBody3D
 
 @onready var ray = $RayCast3D
+@onready var groundCheck = $AreaBody3D
 
-var AccellShiftOverSpeed : float
 @export var acc : float = 0.5
 @export var dec : float = 1.05
 @export var topSpd : float = 15
 @export var maxSpd : float = 30
 @export var maxFallSpd : float = 12
 @export var slopeFactor : float = 2
+@export var AccellShiftOverSpeed : float
+@export var TangentialDragShiftSpeed : float
+@export var TangentialDrag : float
+@export var SlopePowerShiftSpeed : float
+@export var accoverSpd : Curve
+@export var tangentDragOverSpd : Curve
+@export var SlopePowerOverSpeed : Curve
 var LandingConversionFactor : float = 2
 var slopeSpdLimit : float = 10
 var SlopeRunningAngleLimit : float = 0.5
@@ -16,11 +23,19 @@ var GroundStickingPower : float = -1
 var slopeStandingLimit : float = 0.8
 var StartDownhillMultiplier : float = -7
 var UphillMultiplier : float = 0.5
+var DownhillMultiplier : float  = 2
+var RollingUphillBoost : float
 var keepNormal : Vector3
 var keepNormalCounter : float
 
+
 var curvePosAcc : float
+var curvePosTang : float
+var curvePosSlope : float
 var speedMagnitude : float
+var b_normalSpeed : float
+var b_normalVelocity : Vector3
+var b_tangentVelocity : Vector3
 
 var onGround : bool
 var wasOnAir : bool
@@ -65,7 +80,9 @@ func generalPhysics() -> void:
 	if rawInput.length_squared() >= 0.9:
 		previousInput = rawInput
 		
-	curvePosAcc = lerp(curvePosAcc, ((linear_velocity.length() / maxSpd)/maxSpd), get_process_delta_time() * AccellShiftOverSpeed )
+	curvePosAcc = lerp(curvePosAcc, accoverSpd.sample((linear_velocity.length_squared() / maxSpd)/maxSpd), get_physics_process_delta_time() * AccellShiftOverSpeed)
+	curvePosTang = lerp(curvePosTang, tangentDragOverSpd.sample((linear_velocity.length_squared()/ maxSpd)/maxSpd), get_physics_process_delta_time() * TangentialDragShiftSpeed)
+	curvePosSlope = lerp(curvePosSlope, SlopePowerOverSpeed.sample((linear_velocity.length_squared()/maxSpd)/maxSpd), get_physics_process_delta_time() * SlopePowerShiftSpeed)
 	
 	var XZmag = Vector3(linear_velocity.x, 0, linear_velocity.z).length()
 	
@@ -97,43 +114,39 @@ func generalPhysics() -> void:
 		onGround = false
 		groundMovement()
 		#print("false")
-		
+	print(groundCheck)
 	
-	if onGround:
-		rotation = Quaternion(Vector3.UP, groundNormal) * rotation
-		
-		keepNormal = groundNormal
-		keepNormalCounter = 0
-	else:
-		keepNormalCounter += 1
-		if (keepNormalCounter < 5):
-			rotation = Quaternion(Vector3.UP, keepNormal) * rotation
-		else:
-			rotation = Vector3(0, rotation.y, 0)
+	#if onGround:
+		#rotation = Quaternion(transform.basis.y, groundNormal) * rotation
+		#
+		#keepNormal = groundNormal
+		#keepNormalCounter = 0
+	#else:
+		#keepNormalCounter += 1
+		#if (keepNormalCounter < 5):
+			#rotation = Quaternion(transform.basis.y, keepNormal) * rotation
+		#else:
+			#rotation = Vector3(0, rotation.y, 0)
 	
 	print("rotation:", rotation)
 	
 func groundControl(delta : float, input : Vector3):
 	print("GC true")
 	
-	var inputDir = input.normalized()
-	var inputMagnitude = input.length()
-	
-	var velocity = linear_velocity
-	var localVelocity = global_transform.basis.inverse() * velocity
-	
-	var lateralVelocity = Vector3(localVelocity.x, 0, localVelocity.z)
-	var verticalVelocity = Vector3(0, localVelocity.y, 0)
-	
-	var normalSpd = lateralVelocity.dot(inputDir)
-	var normalVelocity = inputDir * normalSpd
-	var tangetVelocity = lateralVelocity - normalVelocity
-	
-	var curvePosTang = (linear_velocity.length_squared() / maxSpd) / maxSpd
-	
 	if (input.length_squared() != 0):
 		
-		tangetVelocity = tangetVelocity.move_toward(Vector3.ZERO, (curvePosTang) * delta * inputMagnitude)
+		var inputDir = input.normalized()
+		var inputMagnitude = input.length()
+		
+		var velocity = linear_velocity
+		var localVelocity = global_transform.basis.inverse() * velocity
+		
+		var lateralVelocity = Vector3(localVelocity.x, 0, localVelocity.z)
+		var verticalVelocity = Vector3(0, localVelocity.y, 0)
+		
+		var normalSpd = lateralVelocity.dot(inputDir)
+		var normalVelocity = inputDir * normalSpd
+		var tangetVelocity = lateralVelocity - normalVelocity
 		
 		if (normalSpd < topSpd):
 			normalSpd += acc * delta * inputMagnitude
@@ -145,25 +158,33 @@ func groundControl(delta : float, input : Vector3):
 			else:
 				normalVelocity = inputDir * normalSpd
 		
+		var curvePosTang : float = (linear_velocity.length_squared() / maxSpd) / maxSpd
+		
+		tangetVelocity = tangetVelocity.move_toward(Vector3.ZERO, (TangentialDrag * tangentDragOverSpd.sample(curvePosTang)) * delta * inputMagnitude)
+		
 		localVelocity = normalVelocity + tangetVelocity + verticalVelocity
 		velocity = global_transform.basis * localVelocity
 		linear_velocity = velocity
 		
-	print("Input Direction: ", inputDir)
-	print("input magnitude: ", inputMagnitude)
-	#print("velocity: ", velocity)
-	#print("localVelocity: ", localVelocity)
-	#print("lateralVelocity: ", lateralVelocity)
-	#print("verticalVelocity: ", verticalVelocity)
-	#print("normalSpd: ", normalSpd)
-	#print("normalVelocity: ", normalVelocity)
-	#print("tangetVelocity: ", tangetVelocity)
+		b_normalSpeed = normalSpd
+		b_normalVelocity = normalVelocity
+		b_tangentVelocity = tangetVelocity
+		
+		print("Input Direction: ", inputDir)
+		print("input magnitude: ", inputMagnitude)
+		#print("velocity: ", velocity)
+		#print("localVelocity: ", localVelocity)
+		#print("lateralVelocity: ", lateralVelocity)
+		#print("verticalVelocity: ", verticalVelocity)
+		#print("normalSpd: ", normalSpd)
+		#print("normalVelocity: ", normalVelocity)
+		#print("tangetVelocity: ", tangetVelocity)
 
 func groundMovement():
 	print("GM true")
 	slopePhysics()
 	
-	groundControl(1, moveInput * get_physics_process_delta_time())
+	groundControl(1, moveInput * curvePosAcc)
 	
 	if (moveInput == Vector3.ZERO):
 		linear_velocity = linear_velocity / dec
@@ -185,19 +206,29 @@ func slopePhysics():
 	
 	if (linear_velocity.length_squared() < slopeSpdLimit and SlopeRunningAngleLimit > groundNormal.y):
 		transform.basis = Quaternion.IDENTITY
+		print("quant:", Quaternion.IDENTITY)
 		addVelocity(groundNormal * 3)
 	else:
 		stickToground(GroundStickingPower)
 	
 	if (onGround and groundNormal.y < slopeStandingLimit):
 		if linear_velocity.y > StartDownhillMultiplier:
-			var force : Vector3 = Vector3(0, (slopeFactor * get_process_delta_time()), 0)
+			var force : Vector3 = Vector3(0, (slopeFactor * curvePosSlope) * UphillMultiplier, 0) * RollingUphillBoost
+			addVelocity(force)
+			
+		if (moveInput != Vector3.ZERO and b_normalSpeed > 0):
+			var force : Vector3 = Vector3(0, (slopeFactor * curvePosSlope) * DownhillMultiplier , 0)
+			addVelocity(force)
+		else:
+			var force : Vector3 = Vector3(0, (slopeFactor * curvePosSlope) , 0)
 			addVelocity(force)
 
 func stickToground(stickingPower : float):
-#	
-	look_at(position)
-	if ray.is_colliding():
+	
+	ray.look_at(ray.get_collision_point(), position)
+	#look_at(ray.get_collision_normal(), Vector3.UP)
+	
+	if ray.is_colliding() and not Input.is_action_pressed("jump"):
 		var force : Vector3 = ray.get_collision_normal() * stickingPower
 		addVelocity(force)
 
